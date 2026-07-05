758c6fa49f1fe04a0972ffc553be226e2cf459e9
Roham Tehrani <roham.rafii@gmail.com>
2026-07-06 00:40:27 +0330

    add stepper to ui

---
+++ projects/ui/src/lib/stepper/stepper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-stepper',
+  standalone: true,
+  imports: [],
+  templateUrl: './stepper.component.html',
+  styleUrl: './stepper.component.css'
+})
+export class StepperComponent {
+
+}
