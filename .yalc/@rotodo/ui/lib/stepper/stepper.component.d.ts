aef7db77d0c34fce28b1253ec88558a1e8e614ce
Roham Tehrani <roham.rafii@gmail.com>
2026-07-06 00:41:11 +0330

    change version to 0.0.2

---
+++ .yalc/@rotodo/ui/lib/stepper/stepper.component.d.ts
@@ -0,0 +1,5 @@
+import * as i0 from "@angular/core";
+export declare class StepperComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<StepperComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<StepperComponent, "lib-stepper", never, {}, {}, never, never, true, never>;
+}
--- .yalc/@rotodo/ui/public-api.d.ts
+++ .yalc/@rotodo/ui/public-api.d.ts
@@ -1,3 +1,4 @@
 export * from './lib/ui.service';
 export * from './lib/ui.component';
 export * from './lib/toggle-switch/toggle-switch.component';
+export * from './lib/stepper/stepper.component';
